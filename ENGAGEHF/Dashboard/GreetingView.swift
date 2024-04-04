17937fdaad52b6df60d455119c54440c9d2359bf
nriedman <108841122+nriedman@users.noreply.github.com>
2024-04-04 10:48:23 -0700

    Update GreetingView.swift

--- ENGAGEHF/Dashboard/GreetingView.swift
+++ ENGAGEHF/Dashboard/GreetingView.swift
@@ -5,40 +5,20 @@
 //
 // SPDX-License-Identifier: MIT
 //
-//  GreetingView.swift
-//  ENGAGEHF
-//
-//  Created by Nick Riedman on 4/4/24.
-//
 
 import SwiftUI
 
+
 struct Greeting: View {
-    @State private var dateString: String?
-    
     var body: some View {
-        HStack {
-            // Todo: replace world! with first name from account info
+        HStack(alignment: .top) {
             Text("Hello, world!")
-                .font(.title2)
-                .accessibilityLabel(Text("DASHBOARD_GREETING"))
+                .font(.title.bold())
             Spacer()
-            Text(dateString ?? "No date")
-                .font(.title2)
-                .accessibilityLabel(Text("DASHBOARD_DATE"))
+            Text(.now, style: .date)
+                .font(.title3)
+                .foregroundStyle(.secondary)
         }
         .padding()
-        .task {
-            getDateString()
-        }
-    }
-    
-    private func getDateString() {
-        let currentDate = Date()
-        let dateFormatter = DateFormatter()
-        
-        dateFormatter.dateStyle = .short
-        
-        dateString = dateFormatter.string(from: currentDate)
     }
 }
