c7e125164c45a4ec65246b013d8805ae383a8a9c
nriedman <108841122+nriedman@users.noreply.github.com>
2024-09-03 14:28:02 -0700

    Start testing, get rid of deprecated firebase

---
+++ ENGAGEHFUITests/Dashboard/MessagesUITestsWithEmulator.swift
@@ -0,0 +1,30 @@
+//
+// This source file is part of the ENGAGE-HF project based on the Stanford Spezi Template Application project
+//
+// SPDX-FileCopyrightText: 2024 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import XCTest
+
+
+final class MessagesUITestsWithEmulator: XCTestCase {
+    override func setUpWithError() throws {
+        try super.setUpWithError()
+        
+        continueAfterFailure = false
+
+        let app = XCUIApplication()
+        app.launchArguments = [
+            "--assumeOnboardingComplete",
+            "--setupTestEnvironment",
+            "--useFirebaseEmulator"
+        ]
+        app.launch()
+    }
+
+    func testMessagesFromEmulator() throws {
+        
+    }
+}
