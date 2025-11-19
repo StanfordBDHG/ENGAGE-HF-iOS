b9afa85376b5042ec74fd2e6837797bcc8f53852
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2025-11-19 20:05:45 +0100

    only update actual source files

--- ENGAGEHF/Education/VideoPlayers/VideoPlayer.swift
+++ ENGAGEHF/Education/VideoPlayers/VideoPlayer.swift
@@ -11,13 +11,23 @@ import SwiftUI
 
 
 struct VideoPlayer: View {
+    // swiftlint:disable:next force_unwrapping
+    private static let youtubeEmbedUrl = URL(string: "https://youtube.com/embed")!
+    
     let youtubeId: String
     
     @State private var viewState: ViewState = .processing
     
+    private var request: URLRequest {
+        let url = VideoPlayer.youtubeEmbedUrl.appending(component: youtubeId)
+        var request = URLRequest(url: url)
+        let referer = Bundle.main.bundleIdentifier.map { "https://\($0)" } ?? "https://localhost"
+        request.setValue(referer, forHTTPHeaderField: "Referer")
+        return request
+    }
     
     var body: some View {
-        WebView(urlString: "https://youtube.com/embed/\(youtubeId)", viewState: $viewState)
+        WebView(request: request, viewState: $viewState)
             .aspectRatio(16 / 9, contentMode: .fit)
             .overlay {
                 if viewState == .processing {
