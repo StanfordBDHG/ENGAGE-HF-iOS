7e788cf5108fed5c98052b3b6b3fffefe1f5168b
Paul Kraft <pauljohanneskraft@users.noreply.github.com>
2026-02-03 20:24:21 +0100

    Increase wait times and make sure to dismiss the save password alert

--- ENGAGEHFUITests/Account/OnboardingUITests.swift
+++ ENGAGEHFUITests/Account/OnboardingUITests.swift
@@ -128,6 +128,12 @@ extension XCUIApplication {
         collectionViews.buttons["Signup"].tap()
         
         sleep(3)
+        
+        let passwordSaveNotNowButton =
+            scrollViews.otherElements.buttons["Not Now"]
+        if passwordSaveNotNowButton.waitForExistence(timeout: 2) {
+            passwordSaveNotNowButton.tap()
+        }
     }
     
     private func navigateInvitationCode(code: String) throws {
